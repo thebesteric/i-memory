3c780a6e686d77e4c942374d4e4ef6708ac6169a
wesley.wang <wesley.wang@quectel.com>
2026-03-11 18:28:39 +0800

    perf: 增加命令行参数获取数据集位置

--- src/ai/model/bert_train.py
+++ src/ai/model/bert_train.py
@@ -1,9 +1,22 @@
+import argparse
+
 from src.ai.model.bert_manager import BertManager, BertIncrModel, LabelConfig, LabelBranchConfig
 
 if __name__ == '__main__':
+
+    # 从命令行获取 model_name_or_path
+    parser = argparse.ArgumentParser(description='BERT 模型训练脚本 - 从命令行指定模型路径/名称')
+    parser.add_argument(
+        '--dataset_path',  # 参数名（命令行使用 --xxx 形式）
+        type=str,  # 参数类型
+        required=True,  # 设置为必选参数
+        help='数据集路径'
+    )
+    args = parser.parse_args()
+    dataset_path = args.dataset_path or "/Users/wangweijun/PycharmProjects/i-memory/assets/datasets"
+
     manager = BertManager(model_name_or_path="google-bert/bert-base-multilingual-cased")
 
-    dataset_path = "/Users/wangweijun/PycharmProjects/i-memory/assets/datasets"
     train_dataset = manager.load_dataset(dataset_path, load_type="json", split="train")
     valid_dataset = manager.load_dataset(dataset_path, load_type="json", split="validation")
 
